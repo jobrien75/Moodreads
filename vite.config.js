8ea9359ec48dbab18e7dc08b403157b07dffb2e1
jobrien75 <102331574+jobrien75@users.noreply.github.com>
2025-08-10 21:32:00 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects', // Specify the source file
+          dest: '.' // Copy it to the root of `dist`
+        }
+      ]
+    })
+  ],
+  server: {
+    open: true,
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  preview: {
+    historyApiFallback: true,
+  },
+});
